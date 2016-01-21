28e0b54bf7fef89676127eab88011f1171711f05
Cyril Moron <cyril@moron.at>
2016-01-21 23:38:02 +0100

    Adding admin role in user model

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,9 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+  before_filter do 
+    redirect_to :new_user_session_path unless current_user && current_user.admin?
+  end
 
   # GET /ingredients
   # GET /ingredients.json
